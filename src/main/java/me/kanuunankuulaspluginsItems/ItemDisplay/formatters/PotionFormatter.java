b1c2c6d8f3fec41cc1d0af863f89c524a1af30b7
If-Master <if.masters.kan@gmail.com>
2025-10-30 22:00:05 +0200

    Update PotionFormatter.java

--- src/main/java/me/kanuunankuulaspluginsItems/ItemDisplay/formatters/PotionFormatter.java
+++ src/main/java/me/kanuunankuulaspluginsItems/ItemDisplay/formatters/PotionFormatter.java
@@ -6,51 +6,40 @@ import org.bukkit.potion.PotionType;
 import org.bukkit.potion.PotionData;
 
 public class PotionFormatter {
+    private static final String[] ROMAN_NUMERALS = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 
     public static String formatPotionEffect(PotionEffect effect) {
-        StringBuilder effectText = new StringBuilder();
-
-        String effectName = formatPotionEffectName(effect.getType());
-        effectText.append(effectName);
+        StringBuilder sb = new StringBuilder();
+        sb.append(formatPotionEffectName(effect.getType()));
 
         if (effect.getAmplifier() > 0) {
-            effectText.append(" ").append(toRomanNumeral(effect.getAmplifier() + 1));
+            sb.append(" ").append(toRomanNumeral(effect.getAmplifier() + 1));
         }
 
         int seconds = effect.getDuration() / 20;
-        int minutes = seconds / 60;
-        seconds = seconds % 60;
-        effectText.append(" (").append(minutes).append(":").append(String.format("%02d", seconds)).append(")");
+        sb.append(" (").append(seconds / 60).append(":").append(String.format("%02d", seconds % 60)).append(")");
 
-        return effectText.toString();
+        return sb.toString();
     }
 
     public static String formatPotionDataWithDuration(PotionData data) {
-        StringBuilder effectText = new StringBuilder();
-        PotionType type = data.getType();
-
-        String effectName = getPotionEffectName(type);
+        String effectName = getPotionEffectName(data.getType());
         if (effectName.isEmpty()) {
             return formatPotionData(data);
         }
 
-        effectText.append(effectName);
+        StringBuilder sb = new StringBuilder(effectName);
+        if (data.isUpgraded()) sb.append(" II");
 
-        if (data.isUpgraded()) {
-            effectText.append(" II");
-        }
-
-        String duration = getPotionDuration(type, data.isExtended(), data.isUpgraded());
+        String duration = getPotionDuration(data.getType(), data.isExtended(), data.isUpgraded());
         if (!duration.isEmpty()) {
-            effectText.append(" (").append(duration).append(")");
+            sb.append(" (").append(duration).append(")");
         }
 
-        return effectText.toString();
+        return sb.toString();
     }
 
     public static String formatPotionTypeWithDuration(PotionType type) {
-        StringBuilder effectText = new StringBuilder();
-
         String keyName = type.getKey().getKey();
         boolean isStrong = keyName.startsWith("strong_");
         boolean isLong = keyName.startsWith("long_");
@@ -60,18 +49,15 @@ public class PotionFormatter {
             return formatPotionTypeName(type);
         }
 
-        effectText.append(effectName);
-
-        if (isStrong) {
-            effectText.append(" II");
-        }
+        StringBuilder sb = new StringBuilder(effectName);
+        if (isStrong) sb.append(" II");
 
         String duration = getPotionDuration(type, isLong, isStrong);
         if (!duration.isEmpty()) {
-            effectText.append(" (").append(duration).append(")");
+            sb.append(" (").append(duration).append(")");
         }
 
-        return effectText.toString();
+        return sb.toString();
     }
 
     public static String formatPotionTypeName(PotionType type) {
@@ -84,59 +70,35 @@ public class PotionFormatter {
         boolean isStrong = name.startsWith("strong_");
         boolean isLong = name.startsWith("long_");
 
-        if (isStrong) {
-            name = name.substring(7);
-        } else if (isLong) {
-            name = name.substring(5);
-        }
+        if (isStrong) name = name.substring(7);
+        else if (isLong) name = name.substring(5);
 
-        name = name.replace("_", " ");
-        String[] words = name.split(" ");
-        StringBuilder result = new StringBuilder();
-        for (String word : words) {
+        StringBuilder sb = new StringBuilder();
+        for (String word : name.split("_")) {
             if (!word.isEmpty()) {
-                result.append(Character.toUpperCase(word.charAt(0)))
+                sb.append(Character.toUpperCase(word.charAt(0)))
                         .append(word.substring(1))
                         .append(" ");
             }
         }
 
-        String formatted = result.toString().trim();
-
-        if (isStrong) {
-            formatted += " II";
-        } else if (isLong) {
-            formatted += " (Extended)";
-        }
-
+        String formatted = sb.toString().trim();
+        if (isStrong) return formatted + " II";
+        if (isLong) return formatted + " (Extended)";
         return formatted;
     }
 
     private static String formatPotionData(PotionData data) {
-        StringBuilder effectText = new StringBuilder();
-
-        String typeName = formatPotionTypeName(data.getType());
-        effectText.append(typeName);
-
-        if (data.isUpgraded()) {
-            effectText.append(" II");
-        }
-
-        if (data.isExtended()) {
-            effectText.append(" (Extended)");
-        }
-
-        return effectText.toString();
+        StringBuilder sb = new StringBuilder(formatPotionTypeName(data.getType()));
+        if (data.isUpgraded()) sb.append(" II");
+        if (data.isExtended()) sb.append(" (Extended)");
+        return sb.toString();
     }
 
     private static String getPotionEffectName(PotionType type) {
         String keyName = type.getKey().getKey();
-
-        if (keyName.startsWith("strong_")) {
-            keyName = keyName.substring(7);
-        } else if (keyName.startsWith("long_")) {
-            keyName = keyName.substring(5);
-        }
+        if (keyName.startsWith("strong_")) keyName = keyName.substring(7);
+        else if (keyName.startsWith("long_")) keyName = keyName.substring(5);
 
         switch (keyName) {
             case "regeneration": return "Regeneration";
@@ -159,89 +121,44 @@ public class PotionFormatter {
     }
 
     private static String formatPotionEffectName(PotionEffectType type) {
-        String name = type.getName().toLowerCase().replace("_", " ");
-        String[] words = name.split(" ");
-        StringBuilder result = new StringBuilder();
-        for (String word : words) {
+        StringBuilder sb = new StringBuilder();
+        for (String word : type.getName().toLowerCase().split("_")) {
             if (!word.isEmpty()) {
-                result.append(Character.toUpperCase(word.charAt(0)))
+                sb.append(Character.toUpperCase(word.charAt(0)))
                         .append(word.substring(1))
                         .append(" ");
             }
         }
-        return result.toString().trim();
+        return sb.toString().trim();
     }
 
     private static String getPotionDuration(PotionType type, boolean extended, boolean upgraded) {
         String keyName = type.getKey().getKey();
+        if (keyName.startsWith("strong_")) keyName = keyName.substring(7);
+        else if (keyName.startsWith("long_")) keyName = keyName.substring(5);
 
-        if (keyName.startsWith("strong_")) {
-            keyName = keyName.substring(7);
-        } else if (keyName.startsWith("long_")) {
-            keyName = keyName.substring(5);
-        }
+        if (keyName.equals("healing") || keyName.equals("harming")) return "";
 
-        if (keyName.equals("healing") || keyName.equals("harming")) {
-            return "";
-        }
+        int duration = getDurationSeconds(keyName, extended, upgraded);
+        if (duration == 0) return "";
 
-        int duration = 0;
+        return (duration / 60) + ":" + String.format("%02d", duration % 60);
+    }
 
+    private static int getDurationSeconds(String keyName, boolean extended, boolean upgraded) {
         switch (keyName) {
-            case "regeneration":
-                duration = upgraded ? 22 : (extended ? 90 : 45);
-                break;
-            case "swiftness":
-                duration = extended ? 480 : 180;
-                break;
-            case "fire_resistance":
-                duration = extended ? 480 : 180;
-                break;
-            case "poison":
-                duration = upgraded ? 21 : (extended ? 90 : 45);
-                break;
-            case "night_vision":
-                duration = extended ? 480 : 180;
-                break;
-            case "weakness":
-                duration = extended ? 240 : 90;
-                break;
-            case "strength":
-                duration = extended ? 480 : 180;
-                break;
-            case "slowness":
-                duration = upgraded ? 20 : (extended ? 240 : 90);
-                break;
-            case "leaping":
-                duration = extended ? 480 : 180;
-                break;
-            case "water_breathing":
-                duration = extended ? 480 : 180;
-                break;
-            case "invisibility":
-                duration = extended ? 480 : 180;
-                break;
-            case "slow_falling":
-                duration = extended ? 240 : 90;
-                break;
-            case "turtle_master":
-                duration = upgraded ? 20 : (extended ? 40 : 20);
-                break;
-            default:
-                return "";
+            case "regeneration": return upgraded ? 22 : (extended ? 90 : 45);
+            case "swiftness", "invisibility", "water_breathing", "leaping", "strength", "night_vision",
+                 "fire_resistance": return extended ? 480 : 180;
+            case "poison": return upgraded ? 21 : (extended ? 90 : 45);
+            case "weakness", "slow_falling": return extended ? 240 : 90;
+            case "slowness": return upgraded ? 20 : (extended ? 240 : 90);
+            case "turtle_master": return upgraded ? 20 : (extended ? 40 : 20);
+            default: return 0;
         }
-
-        int minutes = duration / 60;
-        int seconds = duration % 60;
-        return minutes + ":" + String.format("%02d", seconds);
     }
 
     private static String toRomanNumeral(int number) {
-        if (number <= 0) return "";
-        String[] romanNumerals = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        if (number < romanNumerals.length) {
-            return romanNumerals[number];
-        }
-        return String.valueOf(number);
+        return (number > 0 && number < ROMAN_NUMERALS.length) ? ROMAN_NUMERALS[number] : String.valueOf(number);
     }
 }
